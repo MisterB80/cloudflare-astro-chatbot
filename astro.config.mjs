accd7a2ebbf0e1bee8a018a8d1fa04c8b85879eb
MisterB80 <nbalcombe@gmail.com>
2024-12-11 17:06:50 +0000

    Added clerk auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import cloudflare from '@astrojs/cloudflare';
-
 import tailwind from '@astrojs/tailwind';
-
+import clerk from "@clerk/astro";
 import react from '@astrojs/react';
 
 // https://astro.build/config
@@ -17,7 +15,7 @@ export default defineConfig({
     }
   }),
 
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), clerk()],
   vite: {
     ssr: {
       external: ['pdf-parse'], // Exclude pdf-parse from the build
